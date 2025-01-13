b640edbb5975b28142d538976621317f949462ad
Oyinebiladou <oyinebiladouomemu@gmail.com>
2025-01-13 12:19:37 +0100

    added response model

--- routers/user.py
+++ routers/user.py
@@ -36,22 +36,26 @@ def delete_user(user_id: int, current_user: User = Depends(verify_token)):
     
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.put("/users/{user_id}", status_code=status.HTTP_202_ACCEPTED)
+@router.put("/users/{user_id}", response_model=UserResponse, status_code=status.HTTP_202_ACCEPTED)
 def update_user(user_id: int, user: User, current_user: User = Depends(verify_token)):
     hashed_password = hash_password(user.password)
     cursor.execute (" UPDATE users SET username = %s, email = %s, password = %s WHERE id = %s RETURNING *", (user.username, user.email, hashed_password, str(user_id)))
     updated_user = cursor.fetchone()
     conn.commit()
-
+    
     if updated_user == None:
         raise HTTPException(status_code=404, detail=f"User {user_id} does not exist")
     
-    return {"updated user": updated_user}
+    user = {"email": updated_user[0], "username": updated_user[1], "id": updated_user[3]}
+    return user 
 
-@router.get("/users/{user_id}")
+@router.get("/users/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
 def getone_user(user_id: int, current_user: User = Depends(verify_token)):
     cursor.execute(" SELECT * FROM users WHERE id = %s ", [user_id], binary=True)
     single_user = cursor.fetchone()
+
     if single_user == None:
         raise HTTPException(status_code=404, detail="User not found")
-    return {"user": single_user}
+    
+    user = {"email": single_user[0], "username": single_user[1], "id": single_user[3]}
+    return user  
